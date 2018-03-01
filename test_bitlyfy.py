fbda5ded247fbf985c07fb47d1fc686a271ca887
Eugene Ha <eha@posteo.de>
2018-03-01 11:53:32 +0100

    Placeholder for tests

---
+++ test_bitlyfy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''
+Tests for Bitlyfy
+'''
+
+import os
+import unittest
+
+from bitlyfy import bitlyfy
+
+class BitlyfyTestCase(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
